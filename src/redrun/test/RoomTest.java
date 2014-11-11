9086a6f64ba47076093f362d4f6900d76b2132e2
jjakenichol <jjaken@unm.edu>
2014-11-11 11:50:34 -0700

    Added test class for Room.java
    Creates a 50x50x100 room.

---
+++ src/redrun/test/RoomTest.java
@@ -0,0 +1,193 @@
+package redrun.test;
+
+import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_COLOR_MATERIAL;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_TEST;
+import static org.lwjgl.opengl.GL11.GL_DIFFUSE;
+import static org.lwjgl.opengl.GL11.GL_LIGHT0;
+import static org.lwjgl.opengl.GL11.GL_LIGHT1;
+import static org.lwjgl.opengl.GL11.GL_LIGHTING;
+import static org.lwjgl.opengl.GL11.GL_LIGHT_MODEL_AMBIENT;
+import static org.lwjgl.opengl.GL11.GL_NORMALIZE;
+import static org.lwjgl.opengl.GL11.GL_POSITION;
+import static org.lwjgl.opengl.GL11.GL_SPECULAR;
+import static org.lwjgl.opengl.GL11.glClear;
+import static org.lwjgl.opengl.GL11.glDisable;
+import static org.lwjgl.opengl.GL11.glEnable;
+import static org.lwjgl.opengl.GL11.glLight;
+import static org.lwjgl.opengl.GL11.glLightModel;
+import static org.lwjgl.opengl.GL11.glLoadIdentity;
+import static org.lwjgl.opengl.GL11.glRotatef;
+
+import java.nio.FloatBuffer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.Sys;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
+import redrun.graphics.camera.Camera;
+import redrun.graphics.selection.Picker;
+import redrun.model.gameobject.world.Room;
+import redrun.model.toolkit.BufferConverter;
+import redrun.model.toolkit.Dimension3D;
+
+/**
+ * A test class for Room.java Creates a 50x50x100 room. Uses our standard
+ * controls for movement.
+ * 
+ * @author JakeNichol
+ * @since 11-11-2014
+ * @version 1.0
+ */
+public class RoomTest
+{
+  /** Time at last frame */
+  long lastFrame;
+
+  /**
+   * Performs OpenGL initialization.
+   */
+  private void initOpenGL()
+  {
+    try
+    {
+      Display.setDisplayMode(new DisplayMode(800, 600));
+      Display.setTitle("An Awesome OpenGL Scene");
+      Display.create();
+      Display.setVSyncEnabled(true);
+    }
+    catch (LWJGLException ex)
+    {
+      Logger.getLogger(GraphicsTestTroy.class.getName()).log(Level.SEVERE, null, ex);
+    }
+
+    glEnable(GL_DEPTH_TEST);
+    glDisable(GL_COLOR_MATERIAL);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glEnable(GL_LIGHT1);
+    glEnable(GL_NORMALIZE);
+  }
+
+  /**
+   * The main loop where the logic occurs. Stopped when the escape key is
+   * pressed or the window is closed.
+   */
+  private void gameLoop()
+  {
+    // Create the camera.
+    Camera camera = new Camera(70, (float) Display.getWidth() / (float) Display.getHeight(), 0.3f, 1000, 0.0f, 0.0f,
+        0.0f);
+
+    // Create the room.
+    Room room = new Room(0, 0, 0, new Dimension3D(50, 50, 100));
+
+    // Used for controlling the camera with the keyboard and mouse...
+    float dx = 0.0f;
+    float dy = 0.0f;
+    float dt = 0.0f;
+
+    // Set the mouse sensitivity...
+    float mouseSensitivity = 0.08f;
+    float movementSpeed = 0.05f;
+
+    // Hide the mouse cursor...
+    Mouse.setGrabbed(true);
+
+    while (!Display.isCloseRequested() && !Keyboard.isKeyDown(Keyboard.KEY_ESCAPE))
+    {
+      dt = this.getDelta();
+
+      dx = Mouse.getDX();
+      dy = Mouse.getDY();
+
+      camera.yaw(dx * mouseSensitivity);
+      camera.pitch(-dy * mouseSensitivity);
+
+      // Controls
+      if (Keyboard.isKeyDown(Keyboard.KEY_W)) camera.moveForward(movementSpeed * dt);
+      if (Keyboard.isKeyDown(Keyboard.KEY_S)) camera.moveBackward(movementSpeed * dt);
+      if (Keyboard.isKeyDown(Keyboard.KEY_A)) camera.moveLeft(movementSpeed * dt);
+      if (Keyboard.isKeyDown(Keyboard.KEY_D)) camera.moveRight(movementSpeed * dt);
+      if (Keyboard.isKeyDown(Keyboard.KEY_UP)) camera.moveUp(movementSpeed * dt);
+      if (Keyboard.isKeyDown(Keyboard.KEY_DOWN)) camera.moveDown(movementSpeed * dt);
+
+      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+      glLoadIdentity();
+      camera.lookThrough();
+
+      // Rotate to look at the scene...
+      glRotatef(135, 0, 1, 0);
+
+      // Add ambient light...
+      FloatBuffer ambientColor = BufferConverter.asFloatBuffer(new float[] { 0.2f, 0.2f, 0.2f, 1.0f });
+      glLightModel(GL_LIGHT_MODEL_AMBIENT, ambientColor);
+
+      // Add positional light...
+      FloatBuffer lightColor0 = BufferConverter.asFloatBuffer(new float[] { 1.5f, 1.5f, 1.5f, 1.0f });
+      FloatBuffer lightPosition0 = BufferConverter.asFloatBuffer(new float[] { 15.0f, 0.0f, 5.0f, 1.0f });
+      glLight(GL_LIGHT0, GL_DIFFUSE, lightColor0);
+      glLight(GL_LIGHT0, GL_SPECULAR, lightColor0);
+      glLight(GL_LIGHT0, GL_POSITION, lightPosition0);
+
+      // Add directional light...
+      FloatBuffer lightColor1 = BufferConverter.asFloatBuffer(new float[] { 0.5f, 0.5f, 0.5f, 1.0f });
+      FloatBuffer lightPosition1 = BufferConverter.asFloatBuffer(new float[] { 0.0f, 15.0f, 0.0f, 0.0f });
+      glLight(GL_LIGHT1, GL_DIFFUSE, lightColor1);
+      glLight(GL_LIGHT1, GL_POSITION, lightPosition1);
+
+      // Draw the checker-board...
+      room.draw();
+
+      Display.update();
+      Display.sync(60);
+    }
+  }
+
+  /**
+   * Calculate how many milliseconds have passed since last frame.
+   * 
+   * @return milliseconds passed since last frame
+   */
+  public int getDelta()
+  {
+    long time = getTime();
+    int delta = (int) (time - lastFrame);
+    lastFrame = time;
+
+    return delta;
+  }
+
+  /**
+   * Get the accurate system time
+   * 
+   * @return The system time in milliseconds
+   */
+  public long getTime()
+  {
+    return (Sys.getTime() * 1000) / Sys.getTimerResolution();
+  }
+
+  /**
+   * Cleans up OpenGL.
+   */
+  private void destroyOpenGL()
+  {
+    Display.destroy();
+  }
+
+  public static void main(String[] args)
+  {
+    RoomTest test = new RoomTest();
+    test.initOpenGL();
+    test.gameLoop();
+    test.destroyOpenGL();
+  }
+}
