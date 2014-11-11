af69a96d9096e6a9135b76a58900fa73848d00ee
Jayson Grace <jayson.e.grace@gmail.com>
2014-11-11 13:25:32 -0700

    Updated gitignore, spec files for db added
    RSpec files for testing database functionality added
    Updated .gitignore for .idea files generated for rspec
    Updated models for redrun DB

--- models/button.rb
+++ models/button.rb
@@ -1,5 +1,7 @@
 class Button < ActiveRecord::Base
-  belongs_to :kiosk
-  belongs_to :trap
+  belongs_to :kiosk, :class_name => 'Kiosk', :foreign_key => 'kiosk_id'
+  belongs_to :trap, :class_name => 'Trap', :foreign_key => 'trap_id'
+  validates_presence_of :kiosk_id
+  validates_presence_of :trap_id
   I18n.enforce_available_locales = true
 end
