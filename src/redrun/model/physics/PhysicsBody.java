934f9ff8d460c9c79124dbe3e77cd02ced1fe5fd
Jordan Medlock <jordanemedlock@gmail.com>
2014-11-20 15:56:15 -0700

    Added convenience methods and classes for physics
    Added Classes PlayerCamera, PhysicsWorld
    Added Methods to Player, PhysicsBody

--- src/redrun/model/physics/PhysicsBody.java
+++ src/redrun/model/physics/PhysicsBody.java
@@ -37,6 +37,7 @@ public class PhysicsBody extends RigidBody
             new Transform(
                 new Matrix4f(direction,PhysicsTools.openGLToBullet(center), 1))), 
         collisionShape);
+    PhysicsWorld.addPhysicsBody(this);
   }
   
   /**
@@ -102,4 +103,72 @@ public class PhysicsBody extends RigidBody
   {
     applyCentralImpulse(PhysicsTools.openGLToBullet(new Vector3f(0,getMass()*7,0))); // about 7 times their mass allows them to jump 2 meters
   }
+  
+  public float getPitch()
+  {
+    trans = this.getMotionState().getWorldTransform(trans);
+    Quat4f q = PhysicsTools.quatFromMatrix(trans.basis);
+    return PhysicsTools.pitchFromQuat(q);
+  }
+  
+  public float getYaw()
+  {
+    trans = this.getMotionState().getWorldTransform(trans);
+    Quat4f q = PhysicsTools.quatFromMatrix(trans.basis);
+    return PhysicsTools.yawFromQuat(q);
+  }
+  
+  public float getRoll()
+  {
+    trans = this.getMotionState().getWorldTransform(trans);
+    Quat4f q = PhysicsTools.quatFromMatrix(trans.basis);
+    return PhysicsTools.rollFromQuat(q);
+  }
+ 
+  public void pitch(float pitch)
+  {
+    this.applyTorque(PhysicsTools.openGLToBullet(new Vector3f(0,pitch,0)));
+  }
+  
+  public void yaw(float yaw)
+  {
+    this.applyTorque(PhysicsTools.openGLToBullet(new Vector3f(0,0,yaw)));
+  }
+
+  public void moveForward(float speed)
+  {
+    trans = this.getMotionState().getWorldTransform(trans);
+    Quat4f q = PhysicsTools.quatFromMatrix(trans.basis);
+    float x = - (float) (Math.sin(PhysicsTools.yawFromQuat(q)) * speed);
+    float y = (float) (Math.cos(PhysicsTools.yawFromQuat(q)) * speed);
+    push(new Vector3f(x,y,0));
+  }
+
+  public void moveBackward(float speed)
+  {
+    trans = this.getMotionState().getWorldTransform(trans);
+    Quat4f q = PhysicsTools.quatFromMatrix(trans.basis);
+    float x = (float) (Math.sin(PhysicsTools.yawFromQuat(q)) * speed);
+    float y = - (float) (Math.cos(PhysicsTools.yawFromQuat(q)) * speed);
+    push(new Vector3f(x,y,0));
+  }
+
+  public void moveLeft(float speed)
+  {
+    trans = this.getMotionState().getWorldTransform(trans);
+    Quat4f q = PhysicsTools.quatFromMatrix(trans.basis);
+    float x = - (float) (Math.cos(PhysicsTools.yawFromQuat(q)) * speed);
+    float y = (float) (Math.sin(PhysicsTools.yawFromQuat(q)) * speed);
+    push(new Vector3f(x,y,0));
+  }
+
+  public void moveRight(float speed)
+  {
+    trans = this.getMotionState().getWorldTransform(trans);
+    Quat4f q = PhysicsTools.quatFromMatrix(trans.basis);
+    float x = (float) (Math.cos(PhysicsTools.yawFromQuat(q)) * speed);
+    float y = - (float) (Math.sin(PhysicsTools.yawFromQuat(q)) * speed);
+    push(new Vector3f(x,y,0));
+  }
+  
 }
