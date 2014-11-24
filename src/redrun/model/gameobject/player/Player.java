ad3f6e40be241f78ac0b28f3f954a64f356ee4da
jjakenichol <jjaken@unm.edu>
2014-11-23 23:57:55 -0700

    Prepared Player for further implementation
    Player framework ready for further developement.
    Added Team enum.  RED, BLUE teams.
    Removed Runner class.
    Removed abstract from Player.

--- src/redrun/model/gameobject/player/Player.java
+++ src/redrun/model/gameobject/player/Player.java
@@ -6,6 +6,7 @@ import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
 
 import redrun.graphics.camera.PlayerCamera;
+import redrun.model.constants.Team;
 import redrun.model.gameobject.GameObject;
 import redrun.model.interactable.Interactable;
 import redrun.model.physics.BoxPhysicsBody;
@@ -13,36 +14,51 @@ import redrun.model.physics.BoxPhysicsBody;
 /**
  * This class represents a player in the game world.
  * 
- * @author Troy Squillaci, Jake Nichol
+ * @author Troy Squillaci, J. Jake Nichol
  * @version 1.0
  * @since 2014-11-07
  */
-public abstract class Player extends GameObject implements Interactable
+public class Player extends GameObject implements Interactable
 {
+  /** This player's camera. */
+  private PlayerCamera camera;
+
+  /** This player's team. */
+  private Team team;
+
+  /** This player's health. */
+  private int health;
+
+  /** This player's lives. */
+  private int lives;
+
+  /** The state of this player's life. */
+  private boolean alive;
+
   /**
    * Creates a new player at the specified position.
    * 
    * @param x the x position of the player
    * @param y the y position of the player
    * @param z the z position of the player
+   * @param name the name of the player
+   * @param textureName the name of the player texture for this player
+   * @param team the team this player is on
    */
-
-  private PlayerCamera camera;
-
   public Player(float x, float y, float z, String name, String textureName, String team)
   {
     super(x, y, z, textureName);
 
     body = new BoxPhysicsBody(new Vector3f(x, y, z), new Vector3f(0.5f, 1.0f, 0.5f), new Quat4f(), 100.0f);
     camera = new PlayerCamera(70, (float) Display.getWidth() / (float) Display.getHeight(), 0.3f, 1000f, x, y, z);
+
     // all the physics you'll need :-D
-    // rigidBody = new BoxRigidBody(this.position, new Vector3f(1,2,1), new
-    // Quat4f(0,0,0,1), 120.0f);
-    // TODO Auto-generated constructor stub
+    // rigidBody = new BoxRigidBody(this.position, new Vector3f(1, 2, 1), new
+    // Quat4f(0, 0, 0, 1), 120.0f);
   }
 
   /**
-   * makes the player jump about 2 meters into the air
+   * Makes the player jump about 2 meters into the air.
    */
   public void jump()
   {
@@ -50,7 +66,7 @@ public abstract class Player extends GameObject implements Interactable
   }
 
   /**
-   * walk in the specified direction. x and z will be normalized
+   * Walk in the specified direction, x and z will be normalized.
    * 
    * @param x
    * @param z
@@ -97,5 +113,98 @@ public abstract class Player extends GameObject implements Interactable
     camera.pitch = body.getPitch();
     camera.yaw = body.getYaw();
     camera.lookThrough();
+  }
+
+  @Override
+  public void interact()
+  {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void update()
+  {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void reset()
+  {
+    // TODO Auto-generated method stub
+
+  }
+
+  /**
+   * Gets the state of this player's life.
+   * 
+   * @return the state of this player's life
+   */
+  public boolean isAlive()
+  {
+    return this.alive;
+  }
+
+  /**
+   * Gets this player's health.
+   * 
+   * @return the health of this player
+   */
+  public int getHealth()
+  {
+    return this.health;
+  }
+
+  /**
+   * Gets this player's team.
+   * 
+   * @return the team of this player
+   */
+  public Team getTeam()
+  {
+    return this.team;
+  }
+
+  /**
+   * Gets this player's lives.
+   * 
+   * @return the number of lives this player has
+   */
+  public int getLives()
+  {
+    return this.lives;
+  }
+
+  /**
+   * Sets the state of this player's life.
+   */
+  public void setAlive(boolean alive)
+  {
+    this.alive = alive;
+  }
+
+  /**
+   * Sets this player's health.
+   */
+  public void setHealth(int health)
+  {
+    this.health = health;
+  }
+
+  /**
+   * Sets this player's team.
+   */
+  public void setTeam(Team team)
+  {
+    this.team = team;
+  }
+
+  /**
+   * Sets this player's lives.
+   */
+  public void setLives(int lives)
+  {
+    this.lives = lives;
   }
 }
