14d963773ad26146bcf69fc9684e294c828df704
jjakenichol <jjaken@unm.edu>
2014-11-23 21:20:54 -0700

    Saving for later

--- src/redrun/model/gameobject/world/Button.java
+++ src/redrun/model/gameobject/world/Button.java
@@ -16,14 +16,16 @@ import org.lwjgl.util.vector.Vector3f;
 public class Button extends WorldObject
 {
   Sphere sphere = new Sphere();
-//  private Vector3f defaultButtonPosition; // See todo below.
+  private Vector3f defaultButtonPosition; // See todo below.
+  private Vector3f currentButtonPosition; // See todo below.
   Vector3f color;
 
   public Button(float x, float y, float z, String textureName, Vector3f color)
   {
     super(x, y, z, textureName);
 
-//    defaultButtonPosition = new Vector3f(x, y, z); // See todo below.
+    defaultButtonPosition = new Vector3f(x, y, z); // See todo below.
+    currentButtonPosition = defaultButtonPosition;
     this.color = color;
 
     sphere.setDrawStyle(GLU.GLU_FILL);
@@ -44,8 +46,8 @@ public class Button extends WorldObject
     if (this.timer.getTime() >= 0.8f) reset();
 
     // TODO: For animation, under construction.
-//    if (timer.isPaused() && this.getY() < defaultButtonPosition.y) this.setY(this.getY() + 0.02f);
-//    else if (!timer.isPaused()) this.setY(this.getY() - 0.02f);
+//    if (timer.isPaused() && this.getY() < defaultButtonPosition.y) this.currentButtonPosition.y = this.getY() + 0.02f;
+//    else if (!timer.isPaused()) this.currentButtonPosition.y = this.getY() - 0.02f;
 
     displayListId = glGenLists(1);
 
@@ -57,12 +59,13 @@ public class Button extends WorldObject
         {
           glBegin(GL_SPHERE_MAP);
           {
+//            glTranslatef(0, defaultButtonPosition.y - currentButtonPosition.y, 0); // See todo above
             if (color != null)
             {
               glColor3f(color.x, color.y, color.z);
               sphere.draw(0.5f, 40, 40);
             }
-            sphere.draw(0.5f, 40, 40);
+            else sphere.draw(0.5f, 40, 40);
           }
           glEnd();
         }
