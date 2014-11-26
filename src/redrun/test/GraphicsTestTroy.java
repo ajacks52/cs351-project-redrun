b562e3ecc11ef1df418ffe6a1dfc26f86ed9c746
Jayson Grace <jayson.e.grace@gmail.com>
2014-11-25 18:09:04 -0700

    Working on experiments
    Experimenting with database code in Troy's GUI test class

--- src/redrun/test/GraphicsTestTroy.java
+++ src/redrun/test/GraphicsTestTroy.java
@@ -1,6 +1,7 @@
 package redrun.test;
 
 import java.nio.FloatBuffer;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -13,6 +14,8 @@ import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.util.Timer;
 import org.newdawn.slick.Color;
 
+import redrun.database.MapObjectDB;
+import redrun.database.RedRunDAO;
 import redrun.graphics.camera.Camera;
 import redrun.graphics.selection.Picker;
 import redrun.model.constants.Direction;
@@ -44,9 +47,9 @@ import static org.lwjgl.opengl.GL11.*;
  */
 public class GraphicsTestTroy
 {
-  //TODO Move this shit.
+  // TODO Move this shit.
   private static long lastFrame;
-  
+
   /** The camera associated with the client. */
   private static Camera camera = null;
 
@@ -66,9 +69,9 @@ public class GraphicsTestTroy
     {
       Logger.getLogger(GraphicsTestTroy.class.getName()).log(Level.SEVERE, null, ex);
     }
-    
+
     camera = new Camera(70, (float) Display.getWidth() / (float) Display.getHeight(), 0.3f, 1000, 0.0f, 0.0f, 0.0f);
-    
+
     glEnable(GL_DEPTH_TEST);
     glDisable(GL_COLOR_MATERIAL);
     glEnable(GL_LIGHTING);
@@ -76,183 +79,219 @@ public class GraphicsTestTroy
     glEnable(GL_LIGHT1);
     glEnable(GL_NORMALIZE);
     glShadeModel(GL_SMOOTH);
-    
+
     FontTools.loadFonts();
   }
 
   /**
-   * The main loop where the logic occurs. Stopped when the escape key is pressed or the window is closed.
+   * The main loop where the logic occurs. Stopped when the escape key is
+   * pressed or the window is closed.
    */
   private static void gameLoop()
-  {    
+  {
     // Create the skybox...
     SkyBox skybox = new SkyBox(0, 0, 0, "blood_sport", camera);
-    
+
     // Create the ground...
     Plane plane = new Plane(0, 0, 0, "flopy12", 1000);
-    
+
     // Create the map...
     LinkedList<MapObject> worldMap = new LinkedList<MapObject>();
-    
-    // Add the starting point...
-    worldMap.add(new Start(0.0f, 0.0f, 0.0f, "brickwall5", Direction.NORTH, null));
-    
-    // Add a walkway...
-    worldMap.add(new Corridor(0.0f, 0.0f, 15.0f, "brickwall5", Direction.NORTH, null));
-    worldMap.add(new Corridor(0.0f, 0.0f, 30.0f, "brickwall5", Direction.NORTH, null));
-    worldMap.add(new Corridor(0.0f, 0.0f, 45.0f, "brickwall5", Direction.NORTH, null));
-    
-    // Add a corner...
-    worldMap.add(new Corner(0.0f, 0.0f, 60.0f, "brickwall5", Direction.NORTH, null));
-    
-    // Add a staircase...
-    worldMap.add(new Staircase(15.0f, 0.0f, 60.0f, "brickwall5", Direction.NORTH, null));
-    
-    // Add a walkway...
-    worldMap.add(new Corridor(30.0f, 15.0f, 60.0f, "brickwall5", Direction.EAST, null));
-    worldMap.add(new Corridor(45.0f, 15.0f, 60.0f, "brickwall5", Direction.EAST, null));
-    
-    // Add a field...
-    worldMap.add(new Field(75.0f, 15.0f, 60.0f, "brickwall5", Direction.EAST, null));
-    
-    // Add a walkway...
-    worldMap.add(new Corridor(105.0f, 15.0f, 60.0f, "brickwall5", Direction.EAST, null));
-    worldMap.add(new Corridor(120.0f, 15.0f, 60.0f, "brickwall5", Direction.EAST, null));
-    
-    // Add a tunnel...
-    worldMap.add(new Tunnel(135.0f, 15.0f, 60.0f, "brickwall5", Direction.EAST, null));
-    worldMap.add(new Tunnel(150.0f, 15.0f, 60.0f, "brickwall5", Direction.EAST, null));
-    
-    // Add a walkway...
-    worldMap.add(new Corridor(165.0f, 15.0f, 60.0f, "brickwall5", Direction.EAST, null));
-    
-    // Add a pit...
-    worldMap.add(new Pit(180.0f, 15.0f, 60.0f, "brickwall5", Direction.EAST, null));
-    
-    // Add a walkway...
-    worldMap.add(new Corridor(195.0f, 15.0f, 60.0f, "brickwall5", Direction.EAST, null));
-    
-    // Add a platform...
-    worldMap.add(new Platform(210.0f, 15.0f, 60.0f, "brickwall5", Direction.EAST, null));
-    
-    // Add the ending point...
-    worldMap.add(new End(225.0f, 15.0f, 60.0f, "brickwall5", Direction.EAST, null));
-    
+
+    ArrayList<MapObjectDB> mapStuff = RedRunDAO.getAllMapObjects();
+
+    for (MapObjectDB object : mapStuff)
+    {
+      
+      System.out.println(object);
+    }
+
+    // // Add the starting point...
+    // worldMap.add(new Start(0.0f, 0.0f, 0.0f, "brickwall5", Direction.NORTH,
+    // null));
+    //
+    // // Add a walkway...
+    // worldMap.add(new Corridor(0.0f, 0.0f, 15.0f, "brickwall5",
+    // Direction.NORTH, null));
+    // worldMap.add(new Corridor(0.0f, 0.0f, 30.0f, "brickwall5",
+    // Direction.NORTH, null));
+    // worldMap.add(new Corridor(0.0f, 0.0f, 45.0f, "brickwall5",
+    // Direction.NORTH, null));
+    //
+    // // Add a corner...
+    // worldMap.add(new Corner(0.0f, 0.0f, 60.0f, "brickwall5", Direction.NORTH,
+    // null));
+    //
+    // // Add a staircase...
+    // worldMap.add(new Staircase(15.0f, 0.0f, 60.0f, "brickwall5",
+    // Direction.NORTH, null));
+    //
+    // // Add a walkway...
+    // worldMap.add(new Corridor(30.0f, 15.0f, 60.0f, "brickwall5",
+    // Direction.EAST, null));
+    // worldMap.add(new Corridor(45.0f, 15.0f, 60.0f, "brickwall5",
+    // Direction.EAST, null));
+    //
+    // // Add a field...
+    // worldMap.add(new Field(75.0f, 15.0f, 60.0f, "brickwall5", Direction.EAST,
+    // null));
+    //
+    // // Add a walkway...
+    // worldMap.add(new Corridor(105.0f, 15.0f, 60.0f, "brickwall5",
+    // Direction.EAST, null));
+    // worldMap.add(new Corridor(120.0f, 15.0f, 60.0f, "brickwall5",
+    // Direction.EAST, null));
+    //
+    // // Add a tunnel...
+    // worldMap.add(new Tunnel(135.0f, 15.0f, 60.0f, "brickwall5",
+    // Direction.EAST, null));
+    // worldMap.add(new Tunnel(150.0f, 15.0f, 60.0f, "brickwall5",
+    // Direction.EAST, null));
+    //
+    // // Add a walkway...
+    // worldMap.add(new Corridor(165.0f, 15.0f, 60.0f, "brickwall5",
+    // Direction.EAST, null));
+    //
+    // // Add a pit...
+    // worldMap.add(new Pit(180.0f, 15.0f, 60.0f, "brickwall5", Direction.EAST,
+    // null));
+    //
+    // // Add a walkway...
+    // worldMap.add(new Corridor(195.0f, 15.0f, 60.0f, "brickwall5",
+    // Direction.EAST, null));
+    //
+    // // Add a platform...
+    // worldMap.add(new Platform(210.0f, 15.0f, 60.0f, "brickwall5",
+    // Direction.EAST, null));
+    //
+    // // Add the ending point...
+    // worldMap.add(new End(225.0f, 15.0f, 60.0f, "brickwall5", Direction.EAST,
+    // null));
+
     Cube testCube = new Cube(0.0f, 50.0f, 0f, "wood");
-    
-//    Corridor corridor1 = new Corridor(20, 0.5f, 20, "wood", Direction.NORTH, null);
-//    Corridor corridor2 = new Corridor(30, 0.5f, 20, "wood", Direction.EAST, null);
-//    Corridor corridor3 = new Corridor(40, 0.5f, 20, "wood", Direction.SOUTH, null);
-//    Corridor corridor4 = new Corridor(50, 0.5f, 20, "wood", Direction.WEST, null);
-//    
-//    Corner corner1 = new Corner(20, 0.5f, 30, "wood", Direction.NORTH, null);
-//    Corner corner2 = new Corner(30, 0.5f, 30, "wood", Direction.EAST, null);
-//    Corner corner3 = new Corner(40, 0.5f, 30, "wood", Direction.SOUTH, null);
-//    Corner corner4 = new Corner(50, 0.5f, 30, "wood", Direction.WEST, null);
-//
-//    End end1 = new End(20, 0.5f, 40, "wood", Direction.NORTH, null);
-//    End end2 = new End(30, 0.5f, 40, "wood", Direction.EAST, null);
-//    End end3 = new End(40, 0.5f, 40, "wood", Direction.SOUTH, null);
-//    End end4 = new End(50, 0.5f, 40, "wood", Direction.WEST, null);
-//    
-//    Start start1 = new Start(20, 0.5f, 50, "wood", Direction.NORTH, null);
-//    Start start2 = new Start(30, 0.5f, 50, "wood", Direction.EAST, null);
-//    Start start3 = new Start(40, 0.5f, 50, "wood", Direction.SOUTH, null);
-//    Start start4 = new Start(50, 0.5f, 50, "wood", Direction.WEST, null);
-//    
-//    Pit pit1 = new Pit(20, 0.5f, 60, "wood", Direction.NORTH, null);
-//    Pit pit2 = new Pit(30, 0.5f, 60, "wood", Direction.EAST, null);
-//    Pit pit3 = new Pit(40, 0.5f, 60, "wood", Direction.SOUTH, null);
-//    Pit pit4 = new Pit(50, 0.5f, 60, "wood", Direction.WEST, null);
-//    
-//    Tunnel tunnel1 = new Tunnel(20, 0.5f, 70, "wood", Direction.NORTH, null);
-//    Tunnel tunnel2 = new Tunnel(30, 0.5f, 70, "wood", Direction.EAST, null);
-//    Tunnel tunnel3 = new Tunnel(40, 0.5f, 70, "wood", Direction.SOUTH, null);
-//    Tunnel tunnel4 = new Tunnel(50, 0.5f, 70, "wood", Direction.WEST, null);
-//    
-//    Staircase stair1 = new Staircase(20, 0.5f, 80, "wood", Direction.NORTH, null);
-//    Staircase stair2 = new Staircase(30, 0.5f, 80, "wood", Direction.EAST, null);
-//    Staircase stair3 = new Staircase(40, 0.5f, 80, "wood", Direction.SOUTH, null);
-//    Staircase stair4 = new Staircase(50, 0.5f, 80, "wood", Direction.WEST, null);
-//    
-//    Platform plat1 = new Platform(20, 0.5f, 90, "wood", null, null);
-//    
-//    Field field1 = new Field(20, 0.5f, 120, "wood", Direction.NORTH, null);
-//    Field field2 = new Field(50, 0.5f, 120, "wood", Direction.EAST, null);
-//    Field field3 = new Field(80, 0.5f, 120, "wood", Direction.SOUTH, null);
-//    Field field4 = new Field(110, 0.5f, 120, "wood", Direction.WEST, null);
-    
+
+    // Corridor corridor1 = new Corridor(20, 0.5f, 20, "wood", Direction.NORTH,
+    // null);
+    // Corridor corridor2 = new Corridor(30, 0.5f, 20, "wood", Direction.EAST,
+    // null);
+    // Corridor corridor3 = new Corridor(40, 0.5f, 20, "wood", Direction.SOUTH,
+    // null);
+    // Corridor corridor4 = new Corridor(50, 0.5f, 20, "wood", Direction.WEST,
+    // null);
+    //
+    // Corner corner1 = new Corner(20, 0.5f, 30, "wood", Direction.NORTH, null);
+    // Corner corner2 = new Corner(30, 0.5f, 30, "wood", Direction.EAST, null);
+    // Corner corner3 = new Corner(40, 0.5f, 30, "wood", Direction.SOUTH, null);
+    // Corner corner4 = new Corner(50, 0.5f, 30, "wood", Direction.WEST, null);
+    //
+    // End end1 = new End(20, 0.5f, 40, "wood", Direction.NORTH, null);
+    // End end2 = new End(30, 0.5f, 40, "wood", Direction.EAST, null);
+    // End end3 = new End(40, 0.5f, 40, "wood", Direction.SOUTH, null);
+    // End end4 = new End(50, 0.5f, 40, "wood", Direction.WEST, null);
+    //
+    // Start start1 = new Start(20, 0.5f, 50, "wood", Direction.NORTH, null);
+    // Start start2 = new Start(30, 0.5f, 50, "wood", Direction.EAST, null);
+    // Start start3 = new Start(40, 0.5f, 50, "wood", Direction.SOUTH, null);
+    // Start start4 = new Start(50, 0.5f, 50, "wood", Direction.WEST, null);
+    //
+    // Pit pit1 = new Pit(20, 0.5f, 60, "wood", Direction.NORTH, null);
+    // Pit pit2 = new Pit(30, 0.5f, 60, "wood", Direction.EAST, null);
+    // Pit pit3 = new Pit(40, 0.5f, 60, "wood", Direction.SOUTH, null);
+    // Pit pit4 = new Pit(50, 0.5f, 60, "wood", Direction.WEST, null);
+    //
+    // Tunnel tunnel1 = new Tunnel(20, 0.5f, 70, "wood", Direction.NORTH, null);
+    // Tunnel tunnel2 = new Tunnel(30, 0.5f, 70, "wood", Direction.EAST, null);
+    // Tunnel tunnel3 = new Tunnel(40, 0.5f, 70, "wood", Direction.SOUTH, null);
+    // Tunnel tunnel4 = new Tunnel(50, 0.5f, 70, "wood", Direction.WEST, null);
+    //
+    // Staircase stair1 = new Staircase(20, 0.5f, 80, "wood", Direction.NORTH,
+    // null);
+    // Staircase stair2 = new Staircase(30, 0.5f, 80, "wood", Direction.EAST,
+    // null);
+    // Staircase stair3 = new Staircase(40, 0.5f, 80, "wood", Direction.SOUTH,
+    // null);
+    // Staircase stair4 = new Staircase(50, 0.5f, 80, "wood", Direction.WEST,
+    // null);
+    //
+    // Platform plat1 = new Platform(20, 0.5f, 90, "wood", null, null);
+    //
+    // Field field1 = new Field(20, 0.5f, 120, "wood", Direction.NORTH, null);
+    // Field field2 = new Field(50, 0.5f, 120, "wood", Direction.EAST, null);
+    // Field field3 = new Field(80, 0.5f, 120, "wood", Direction.SOUTH, null);
+    // Field field4 = new Field(110, 0.5f, 120, "wood", Direction.WEST, null);
+
     // Hide the mouse cursor...
     Mouse.setGrabbed(true);
-    
+
     while (!Display.isCloseRequested() && !Keyboard.isKeyDown(Keyboard.KEY_ESCAPE))
     {
       getInput();
-      
+
       glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
       glLoadIdentity();
-      
+
       glPushMatrix();
       {
         glDepthMask(false);
-        //TODO Fix rotations along X and Z axis.
+        // TODO Fix rotations along X and Z axis.
         glRotatef(camera.getYaw(), 0, 1, 0);
         skybox.draw();
         glDepthMask(true);
       }
       glPopMatrix();
-      
+
       camera.lookThrough();
 
       // Add ambient light...
       FloatBuffer ambientColor = BufferConverter.asFloatBuffer(new float[] { 0.2f, 0.2f, 0.2f, 1.0f });
       glLightModel(GL_LIGHT_MODEL_AMBIENT, ambientColor);
-      
+
       // Add positional light...
       FloatBuffer lightColor0 = BufferConverter.asFloatBuffer(new float[] { 1.5f, 1.5f, 1.5f, 1.0f });
       FloatBuffer lightPosition0 = BufferConverter.asFloatBuffer(new float[] { -15.0f, 10.0f, 5.0f, 1.0f });
       glLight(GL_LIGHT0, GL_DIFFUSE, lightColor0);
       glLight(GL_LIGHT0, GL_SPECULAR, lightColor0);
       glLight(GL_LIGHT0, GL_POSITION, lightPosition0);
-      
+
       // Add directional light...
       FloatBuffer lightColor1 = BufferConverter.asFloatBuffer(new float[] { 0.5f, 0.5f, 0.5f, 1.0f });
       FloatBuffer lightPosition1 = BufferConverter.asFloatBuffer(new float[] { -15.0f, 10.0f, 5.0f, 0.0f });
       glLight(GL_LIGHT1, GL_DIFFUSE, lightColor1);
       glLight(GL_LIGHT1, GL_POSITION, lightPosition1);
-      
+
       if (Keyboard.isKeyDown(Keyboard.KEY_F))
       {
         Picker.startPicking();
         {
-        
+
         }
         Picker.stopPicking();
       }
-      
+
       // Draw the floor...
       plane.draw();
-      
+
       // Draw the world map...
       for (MapObject mapObject : worldMap)
       {
         mapObject.draw();
       }
-      
+
       testCube.draw();
-      
+
       // Draw text to the screen...
       FontTools.draw2D();
-      FontTools.renderText("Position: (" + camera.getX() + ", " + camera.getY() + ", " + camera.getZ() + ")", 10, 10, Color.orange, 1);
+      FontTools.renderText("Position: (" + camera.getX() + ", " + camera.getY() + ", " + camera.getZ() + ")", 10, 10,
+          Color.orange, 1);
       FontTools.draw3D();
-      
+
       PhysicsWorld.stepSimulation(1 / 60.0f);
       Timer.tick();
       Display.update();
       Display.sync(60);
     }
   }
-  
+
   /**
    * Gets user input from the keyboard and mouse.
    */
@@ -262,19 +301,20 @@ public class GraphicsTestTroy
     float dx = 0.0f;
     float dy = 0.0f;
     float dt = 0.0f;
-    
+
     // Set the mouse sensitivity...
     float mouseSensitivity = 0.08f;
     float movementSpeed = 0.02f;
-        
+
     dx = Mouse.getDX();
     dy = Mouse.getDY();
     dt = getDelta();
-    
+
     camera.yaw(dx * mouseSensitivity);
     camera.pitch(-dy * mouseSensitivity);
-    
-    if (Keyboard.isKeyDown(Keyboard.KEY_W) && Keyboard.isKeyDown(Keyboard.KEY_LSHIFT)) camera.moveForward(movementSpeed * dt * 2);
+
+    if (Keyboard.isKeyDown(Keyboard.KEY_W) && Keyboard.isKeyDown(Keyboard.KEY_LSHIFT)) camera.moveForward(movementSpeed
+        * dt * 2);
     else if (Keyboard.isKeyDown(Keyboard.KEY_W)) camera.moveForward(movementSpeed * dt);
     if (Keyboard.isKeyDown(Keyboard.KEY_S)) camera.moveBackward(movementSpeed * dt);
     if (Keyboard.isKeyDown(Keyboard.KEY_A)) camera.moveLeft(movementSpeed * dt);
@@ -282,7 +322,7 @@ public class GraphicsTestTroy
     if (Keyboard.isKeyDown(Keyboard.KEY_UP)) camera.moveUp(movementSpeed * dt);
     if (Keyboard.isKeyDown(Keyboard.KEY_DOWN)) camera.moveDown(movementSpeed * dt);
   }
-  
+
   /**
    * Calculate how many milliseconds have passed since last frame.
    * 
@@ -296,7 +336,6 @@ public class GraphicsTestTroy
 
     return delta;
   }
-
 
   /**
    * Cleans up resources.
