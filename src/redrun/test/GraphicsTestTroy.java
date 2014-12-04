68eebd6a88c1c65504456bdb1168db6c2e721302
Zivia <zivia@unm.edu>
2014-12-03 19:10:10 -0700

    Added Light Source Support for Map File
    A light source can now be specified in the text files that define
    maps.

--- src/redrun/test/GraphicsTestTroy.java
+++ src/redrun/test/GraphicsTestTroy.java
@@ -32,6 +32,7 @@ import redrun.model.gameobject.map.Staircase;
 import redrun.model.gameobject.map.Start;
 import redrun.model.gameobject.map.Tunnel;
 import redrun.model.gameobject.player.Player;
+import redrun.model.gameobject.world.Ball;
 import redrun.model.gameobject.world.Cube;
 import redrun.model.gameobject.world.Plane;
 import redrun.model.gameobject.world.SkyBox;
@@ -275,11 +276,11 @@ public class GraphicsTestTroy
       GameData.addGameObject(new Cube(45.0f, 50.0f + (2 * i), 45.0f, "crate1"));
     }
     
-//    // Create balls above the staircase...
-//    for (int i = 0; i < 10; i++)
-//    {
-//      GameData.addGameObject(new Ball(45.0f, 50.0f + (5 * i), 15.0f, "crate1", 1.5f));
-//    }
+    // Create balls above the staircase...
+    for (int i = 0; i < 10; i++)
+    {
+      GameData.addGameObject(new Ball(45.0f, 50.0f + (5 * i), 15.0f, "crate1", 1.5f));
+    }
         
     // Hide the mouse cursor...
     Mouse.setGrabbed(true);
