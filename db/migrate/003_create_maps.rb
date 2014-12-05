1c29fdcd8249e7d6d6000369c62f48f40bd725d9
Jayson Grace <jayson.e.grace@gmail.com>
2014-12-05 16:59:48 -0700

    Clean up migrations and specs
    Remove unused migrations and spec files associated with those migrations

--- db/migrate/003_create_maps.rb
+++ db/migrate/003_create_maps.rb
@@ -8,9 +8,11 @@ class CreateMaps < ActiveRecord::Migration
       t.column :map_name, :string, null: false, unique: true
       t.column :sky_box, :string, null: false, unique: true
       t.column :floor, :string, null: false, unique: true
+      t.column :light_position, :string, null: false
     end
     add_index :maps, [:map_name], :name => "index_maps_on_map_name"
     add_index :maps, [:sky_box], :name => "index_maps_on_sky_box"
     add_index :maps, [:floor], :name => "index_maps_on_floor"
+    add_index :maps, [:light_position], :name => "index_maps_on_light_position"
   end
 end
