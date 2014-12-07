ceac63f750a55512213364a743a6704411fd4ac0
Jayson Grace <jayson.e.grace@gmail.com>
2014-12-06 23:47:15 -0700

    Updated Main
    Removed TO-DO, set up client to disconnect

--- src/redrun/main/Main.java
+++ src/redrun/main/Main.java
@@ -68,38 +68,39 @@ import redrun.network.UserInput;
 /**
  * This class is where RedRun begins execution.
  * 
- * @author Troy Squillaci, Jake Nichol, Jayson Grace, Adam Michell, Jordan Medlock
+ * @author Troy Squillaci, Jake Nichol, Jayson Grace, Adam Michell, Jordan
+ *         Medlock
  * @version 1.0
  * @since 2014-11-09
  */
 public class Main
 {
   private static boolean running = true;
-  
+
   /** The game menu. */
   private static Menu menu = new Menu();
-  
+
   /** The current game state for this client and its player. */
   private static GameState state = GameState.WAIT;
 
   /** The current game state for this client and its player. */
   private static GameState previousState = GameState.WAIT;
-  
+
   /** The active camera manager. */
   private static CameraManager cameraManager = null;
-    
+
   /** The active camera. */
   private static Camera camera = null;
-  
+
   /** The player associated with the client. */
   private static Player player = null;
-  
+
   /** Used to interface with the network client. */
   private static Client client = null;
 
   /** Used to access the database. */
   private static Map map = null;
-  
+
   private static UserInput input = null;
 
   /**
@@ -109,15 +110,15 @@ public class Main
   {
     // Connect to the server...
     client = new Client("127.0.0.1", 7777);
-    
+
     // Set up the user input...
     input = new UserInput();
-    
+
     // Set up OpenGL...
     try
     {
       Display.setDisplayMode(new DisplayMode(1280, 720));
-      //TODO - Need to have the name of the active map be in the title...
+      // TODO - Need to have the name of the active map be in the title...
       Display.setTitle("RedRun Ice World");
       Display.create();
       Display.setVSyncEnabled(true);
@@ -126,7 +127,7 @@ public class Main
     {
       Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
     }
-    
+
     // Set OpenGL states...
     glEnable(GL_DEPTH_TEST);
     glDisable(GL_COLOR_MATERIAL);
@@ -134,32 +135,34 @@ public class Main
     glEnable(GL_LIGHT0);
     glEnable(GL_NORMALIZE);
     glShadeModel(GL_SMOOTH);
-    
-    //TODO Need to get the player data from the server...
+
+    // TODO Need to get the player data from the server...
     // Set up the cameras...
     player = new Player(0.0f, 1.0f, 0.0f, "Linvala, Keeper of Silence", null, Team.BLUE);
-    
-    Camera spectatorCam = new Camera(70, (float) Display.getWidth() / (float) Display.getHeight(), 0.3f, 1000, 0.0f, 1.0f, 0.0f, CameraType.SPECTATOR);
+
+    Camera spectatorCam = new Camera(70, (float) Display.getWidth() / (float) Display.getHeight(), 0.3f, 1000, 0.0f,
+        1.0f, 0.0f, CameraType.SPECTATOR);
     Camera playerCam = player.getCamera();
-    
+
     cameraManager = new CameraManager(spectatorCam, playerCam);
-    
+
     // Show loading screen...
-//    LoadingScreen.loadingScreen();
+    // LoadingScreen.loadingScreen();
     FontTools.loadFonts();
   }
 
   /**
-   * The main loop where the logic occurs. Stopped when the escape key is pressed or the window is closed.
+   * The main loop where the logic occurs. Stopped when the escape key is
+   * pressed or the window is closed.
    */
   private static void gameLoop()
-  {   
+  {
     // Create the skybox...
     SkyBox skybox = null;
-    
+
     // Create the floor...
     Plane floor = null;
-    
+
     while (!GameData.networkData.isEmpty())
     {
       for (String networkItem : GameData.networkData)
@@ -184,35 +187,36 @@ public class Main
       }
       break;
     }
-    
+
     GameData.bindConnections();
-    
+
     // Create cubes above the staircase...
     for (int i = 0; i < 500; i++)
     {
       GameData.addGameObject(new Cube(45.0f, 50.0f + (2 * i), 45.0f, "crate1"));
     }
-    
+
     // Create balls above the staircase...
     for (int i = 0; i < 10; i++)
     {
-      //GameData.addGameObject(new Ball(45.0f, 50.0f + (5 * i), 15.0f, "crate1", 1.5f));
+      // GameData.addGameObject(new Ball(45.0f, 50.0f + (5 * i), 15.0f,
+      // "crate1", 1.5f));
     }
-            
+
     // Hide the mouse cursor...
     Mouse.setGrabbed(true);
-    
+
     while (!Display.isCloseRequested() && running)
-    {      
+    {
       camera = cameraManager.getActiveCamera();
-        
+
       // Get input from the user...
       getInput();
-      
+
       // Prepare for rendering...
       glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
       glLoadIdentity();
-      
+
       // Draw the skybox...
       glPushMatrix();
       {
@@ -223,27 +227,27 @@ public class Main
         glDepthMask(true);
       }
       glPopMatrix();
-      
+
       // Orient the camera...
       camera.lookThrough();
 
       // Global Ambient Light Model...
       FloatBuffer ambientColor = BufferConverter.asFloatBuffer(new float[] { 0.2f, 0.2f, 0.2f, 1.0f });
       glLightModel(GL_LIGHT_MODEL_AMBIENT, ambientColor);
-      
+
       // Local Viewport Model...
       glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, GL_TRUE);
-      
+
       // Add positional light...
       FloatBuffer lightDiffuse = BufferConverter.asFloatBuffer(new float[] { 1.0f, 1.0f, 1.0f, 1.0f });
       FloatBuffer lightSpecular = BufferConverter.asFloatBuffer(new float[] { 1.0f, 1.0f, 1.0f, 1.0f });
       FloatBuffer lightPosition = BufferConverter.asFloatBuffer(new float[] { -100.0f, 750.0f, 1000.0f, 1.0f });
-      
-      //glLight(GL_LIGHT0, GL_AMBIENT, lightAmibent);
+
+      // glLight(GL_LIGHT0, GL_AMBIENT, lightAmibent);
       glLight(GL_LIGHT0, GL_DIFFUSE, lightDiffuse);
       glLight(GL_LIGHT0, GL_SPECULAR, lightSpecular);
       glLight(GL_LIGHT0, GL_POSITION, lightPosition);
-      
+
       // Picking code for 3D selection of game objects...
       if (Keyboard.isKeyDown(Keyboard.KEY_F))
       {
@@ -254,32 +258,32 @@ public class Main
           {
             glPushName(gameObject.id);
             {
-                gameObject.draw();
+              gameObject.draw();
             }
             glPopName();
           }
         }
         Picker.stopPicking();
       }
-      
+
       // Draw the player...
       player.draw();
-      
+
       // Draw the floor...
       floor.draw();
-      
+
       // Draw the map objects...
       for (MapObject mapObject : GameData.getMapObjects())
       {
         mapObject.draw();
       }
-      
+
       // Draw the game objects...
       for (GameObject gameObject : GameData.getGameObjects())
       {
         gameObject.draw();
       }
-              
+
       // Show the HUD...
       if (state != GameState.MAIN_MENU) HUD_Manager.huds(camera, player);
       // Show the menu...
@@ -289,7 +293,7 @@ public class Main
       Timer.tick();
       Display.update();
       Display.sync(60);
-      
+
       if (ObjectFromDB.mapDrawn == true)
       {
         GameData.networkData.clear();
@@ -297,7 +301,7 @@ public class Main
       }
     }
   }
-  
+
   /**
    * Gets user input from the keyboard and mouse.
    */
@@ -312,33 +316,33 @@ public class Main
       state = GameState.MAIN_MENU;
       menu.setState();
     }
-    
+
     // Used for controlling the camera with the keyboard and mouse...
     float dx = 0.0f;
     float dy = 0.0f;
     float dt = 0.0f;
-    
+
     // Set the mouse sensitivity...
     float mouseSensitivity = 0.08f;
     float movementSpeed = 0.02f;
-        
+
     dx = Mouse.getDX();
     dy = Mouse.getDY();
     dt = Timing.getDelta();
-    
+
     camera.yaw(dx * mouseSensitivity);
     camera.pitch(-dy * mouseSensitivity);
-    
+
     // Camera related input...
     if (Keyboard.isKeyDown(Keyboard.KEY_R))
     {
-        cameraManager.chooseNextCamera();
+      cameraManager.chooseNextCamera();
     }
-    
+
     // Movement related input...
     if (Keyboard.isKeyDown(Keyboard.KEY_W) && Keyboard.isKeyDown(Keyboard.KEY_LSHIFT))
     {
-        camera.moveForward(movementSpeed * dt * 2);
+      camera.moveForward(movementSpeed * dt * 2);
     }
     else if (Keyboard.isKeyDown(Keyboard.KEY_W)) camera.moveForward(movementSpeed * dt);
     if (Keyboard.isKeyDown(Keyboard.KEY_S)) camera.moveBackward(movementSpeed * dt);
@@ -347,7 +351,7 @@ public class Main
     if (Keyboard.isKeyDown(Keyboard.KEY_SPACE)) camera.moveUp(movementSpeed * dt);
     if (Keyboard.isKeyDown(Keyboard.KEY_LCONTROL)) camera.moveDown(movementSpeed * dt);
   }
-  
+
   /**
    * Cleans up resources.
    */
@@ -356,20 +360,19 @@ public class Main
     Display.destroy();
     client.requestDisconnect();
   }
-  
+
   /**
    * Breaks out of the game loop which leads to the program exiting.
    */
   public static void requestClose()
   {
     running = false;
-    //TODO Close network / DB stuff
   }
-  
+
   public static void main(String[] args)
   {
     createResources();
     gameLoop();
-    destroyResources();  
+    destroyResources();
   }
 }
